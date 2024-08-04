0c55361a1473db581224b354fef844174c05e1a4
Pranjal Charde <106790718+TechyNerd201@users.noreply.github.com>
2024-08-04 11:31:28 +0530

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,60 +1,37 @@
 "use strict";
 
-/**
- * Fetch Data from server
- * @param {string} URL - API URL
- * @param {Function} callback - Callback function
- */
-const api_key ="f35433aa09321a9b6b0e8dc439c83693"
+const api_key ="f35433aa09321a9b6b0e8dc439c83693";
+
 export const fetchData = function (URL, callback) {
-  fetch(`${URL}&appid=${api_key}`)
-    .then((response) => response.json())
-    .then((data) => callback(data));
+  fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => callback(data))
+    .catch((error) => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
 };
 
-// const base_url = "http://127.0.0.1:3000";
 const base_url = "https://api.openweathermap.org";
 
 export const url = {
   currentWeather(lat, lon) {
-    return `${base_url}/weather/current?lat=${lat}&lon=${lon}`;
+    return `${base_url}/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`;
   },
   forecast(lat, lon) {
-    return `${base_url}/weather/forecast?lat=${lat}&lon=${lon}`;
+    return `${base_url}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`;
   },
   airPollution(lat, lon) {
-    return `${base_url}/weather/air-pollution?lat=${lat}&lon=${lon}`;
+    return `${base_url}/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}`;
   },
   reverseGeocoding(lat, lon) {
-    return `${base_url}/weather/reverse-geocoding?lat=${lat}&lon=${lon}`;
+    return `${base_url}/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${api_key}`;
   },
-  /**
-   * @param {string} query - City name e.g. London, New York, Tlemcen ...
-   * @returns
-   */
   geocoding(query) {
-    return `${base_url}/weather/geo-coding?query=${query}`;
+    return `${base_url}/geo/1.0/direct?q=${query}&limit=5&appid=${api_key}`;
   },
 };
-
-// export const url = {
-//   currentWeather(lat, lon) {
-//     return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
-//   },
-//   forecast(lat, lon) {
-//     return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`;
-//   },
-//   airPollution(lat, lon) {
-//     return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}`;
-//   },
-//   reverseGeocoding(lat, lon) {
-//     return `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5`;
-//   },
-//   /**
-//    * @param {string} query - City name e.g. London, New York, Tlemcen ...
-//    * @returns
-//    */
-//   geocoding(query) {
-//     return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
-//   },
-// };
